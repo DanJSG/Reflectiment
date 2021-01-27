b250e66482afde889e17a19dac8e8394aad94496
DanJSG <jackson.daniel365@gmail.com>
2021-01-27 13:49:58 +0000

    MENG-25: Removed unneeded builder class

--- lexicalanalyzer/src/main/java/com/dtj503/lexicalanalyzer/types/JsonObjectBuilder.java
+++
@@ -1,8 +0,0 @@
-package com.dtj503.lexicalanalyzer.types;
-
-// TODO review whether this is actually needed
-public interface JsonObjectBuilder<T extends JsonObject> {
-	
-	public T fromJson(String json);
-	
-}
