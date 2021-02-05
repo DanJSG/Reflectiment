405c66a49e2267a56d8d07b6edb381e30f6560f5
DanJSG <jackson.daniel365@gmail.com>
2021-02-05 22:59:50 +0000

    MENG-30: Began commenting score parser

--- lexicalanalyzer/src/main/java/com/dtj503/lexicalanalyzer/sentiment/parsers/ScoreParser.java
+++ lexicalanalyzer/src/main/java/com/dtj503/lexicalanalyzer/sentiment/parsers/ScoreParser.java
@@ -1,7 +1,5 @@
 package com.dtj503.lexicalanalyzer.sentiment.parsers;
 
-
-
 import com.dtj503.lexicalanalyzer.types.Sentence;
 import com.dtj503.lexicalanalyzer.types.Token;
 import com.dtj503.lexicalanalyzer.utils.ListMath;
@@ -10,40 +8,64 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * Class for parsing the sentiment score of a sentence using a lexicon-based approach.
+ * @author
+ */
 public class ScoreParser {
 
+    /**
+     * Method for parsing the scores of all the words in a sentence and calculating the overall sentiment polarity score
+     * for the sentence.
+     *
+     * @param sentence the sentence to parse
+     * @return a <code>float</code> between -1 and 1 (inclusive)
+     */
     public static float parseSentenceScore(Sentence sentence) {
         //TODO remove debugging print statements
         for(Token word : sentence.getWords()) {
             System.out.println(word);
         }
 
+        // Get the adjective, verb and adverb positions from the sentence
         List<Integer> adjectivePositions = sentence.getAdjectivePositions();
         List<Integer> verbPositions = sentence.getVerbPositions();
         List<Integer> adverbPositions = sentence.getAdverbPositions();
+
+        // Get the word scores from the sentence
         List<Float> scores = sentence.getScores();
 
         //TODO remove debugging print statements
         System.out.println(scores);
 
+        // Initialise a list of ones ready to use as a multiplication vector
         List<Float> modificationVector = new ArrayList<>(Collections.nCopies(scores.size(), 1f));
 
+        // If there are adjectives and adverbs in the sentence, then find the adverbs which modify and/or negate the
+        // adjectives and update the modification vector with the modification and negation values
         if(adjectivePositions != null && adjectivePositions.size() > 0 && adverbPositions != null) {
             setModifiersAndNegators(modificationVector, adjectivePositions, adverbPositions, scores);
         }
 
+        // If there are verbs and adverbs in the sentence, then find the adverbs which modify and/or negate the
+        // verbs and update the modification vector with the modification and negation values
         if(verbPositions != null && verbPositions.size() > 0 && adverbPositions != null) {
             setModifiersAndNegators(modificationVector, verbPositions, adverbPositions, scores);
         }
 
+        // Remove the adverb scores from the sentence
         if(adverbPositions != null) {
             scores = removeAdverbs(scores, adverbPositions);
             modificationVector = removeAdverbs(modificationVector, adverbPositions);
         }
 
+        // Calculate the hadamard product of the scores with the modification vector (piecewise multiplication)
         List<Float> modifiedScores = ListMath.hadamardProduct(modificationVector, scores);
+
+        // Remove zero valued scores (unless they are all zero valued)
         modifiedScores = stripZeroScores(modifiedScores);
 
+        // Calculate the overall sentence score by taking the mean of the scores and limiting between -1 and 1
         float sentenceScore = Math.max(-1, Math.min(ListMath.mean(modifiedScores), 1));
 
         //TODO remove debugging print statements
@@ -59,6 +81,12 @@ public class ScoreParser {
 
     }
 
+    /**
+     * Method for removing all zero values from a list, unless the list only contains 0 in which case none are removed.
+     *
+     * @param scores the scores to remove the zeros from
+     * @return array with zeros removed, or original array if it is exclusively zeros
+     */
     private static List<Float> stripZeroScores(List<Float> scores) {
         List<Float> newScores = new ArrayList<>();
         for(float score : scores) {
@@ -69,6 +97,14 @@ public class ScoreParser {
         return newScores.size() > 0 ? newScores : scores;
     }
 
+    /**
+     * Method for finding modifiers and negators of specific words and updating a modification vector with these values.
+     *
+     * @param modificationVector the modification vector to update
+     * @param modifiedPositions the indices of the modified words
+     * @param adverbPositions the indices of the adverbs
+     * @param scores the scores of the words in the sentence
+     */
     private static void setModifiersAndNegators(List<Float> modificationVector, List<Integer> modifiedPositions,
                                                 List<Integer> adverbPositions, List<Float> scores) {
         List<Float> verbNegators = getNegators(modifiedPositions, adverbPositions, scores);
@@ -77,12 +113,22 @@ public class ScoreParser {
         updateModificationVector(modificationVector, modifiedPositions, verbNegators);
     }
 
+    /**
+     * Method for finding the indices of adverb negators from a sentence.
+     *
+     * @param positions the indices of the words being negated
+     * @param adverbPositions the indices of the adverbs
+     * @param scores the scores of the words in the sentence
+     * @return
+     */
     private static List<Float> getNegators(List<Integer> positions, List<Integer> adverbPositions, List<Float> scores) {
         List<Float> negators = new ArrayList<>();
         for(int pos : positions) {
             float currentNegator = 1;
             int prevPos = pos - 1;
+            // Loop backwards from the modified word to find all potential negators
             while(prevPos >= 0 && adverbPositions.contains(prevPos)) {
+                // If the word has a negative polarity, flip the sign
                 if(scores.get(prevPos) < 0) {
                     currentNegator *= -1;
                 }
