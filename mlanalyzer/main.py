cbf55f6acaa508ed8d1b9a5d4d5886223b72d90e
DanJSG <jackson.daniel365@gmail.com>
2021-02-20 14:02:00 +0000

    MENG-52: Added comment to application start method

--- mlanalyzer/main.py
+++ mlanalyzer/main.py
@@ -1,6 +1,8 @@
 import app
 
-def start():
+def start() -> None:
+    """ Initialize and start the Flask web application.
+    """
     print("Function [start()] called.")
     service = app.get_app()
     service.run(host='0.0.0.0', port=8080, use_reloader=False)
