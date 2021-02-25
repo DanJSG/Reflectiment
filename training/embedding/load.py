59cb2b9c76fb60f52c8983d5189572c20d1cbac2
DanJSG <jackson.daniel365@gmail.com>
2021-02-25 19:43:05 +0000

    MENG-59: Added prediction test with and without padding to model load file

--- training/embedding/load.py
+++ training/embedding/load.py
@@ -1,5 +1,17 @@
 import tensorflow as tf
+from keras.preprocessing.sequence import pad_sequences
+from pprint import pprint
 
 model: tf.keras.Sequential = tf.keras.models.model_from_json(open("./embedding.json", "r").read())
 model.load_weights("./embedding.hdf5")
 model.summary()
+
+input = [[152, 412, 542, 111, 53, 5, 353, 8751]]
+padded_input = pad_sequences(input, maxlen=52, padding='post').tolist()
+
+result = model.predict(input)
+padded_result = model.predict(padded_input)
+
+pprint(result.tolist())
+print(len(result.tolist()[0]))
+print(len(padded_result.tolist()[0]))
