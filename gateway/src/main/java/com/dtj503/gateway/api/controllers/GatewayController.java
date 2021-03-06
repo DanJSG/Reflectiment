7d0bce07b8b548ab5908a4a0a285f876baf02380
DanJSG <jackson.daniel365@gmail.com>
2021-03-06 11:15:33 +0000

    MENG-70: Added comments to gateway API rest controller

--- gateway/src/main/java/com/dtj503/gateway/api/controllers/GatewayController.java
+++ gateway/src/main/java/com/dtj503/gateway/api/controllers/GatewayController.java
@@ -6,9 +6,19 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * API controller for handling the text submission, distributing the analysis requests, and combining the responses.
+ *
+ * @author Dan Jackson (dtj503@york.ac.uk)
+ */
 @RestController
 public class GatewayController extends RestAPIController {
 
+    /**
+     * API analysis endpoint method. Called when a POST request is made to /document.
+     *
+     * @param submission the text submission to analyze
+     */
     @PostMapping(value = "/document", consumes = MediaType.APPLICATION_JSON_VALUE)
     public static void analyze(@RequestBody TextSubmission submission) {
         System.out.println(submission.getText());
