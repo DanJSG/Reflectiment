82d29fc41b35671765e96f24ebd141e1ff4418a6
DanJSG <jackson.daniel365@gmail.com>
2021-03-09 10:49:56 +0000

    MENG-68: Trained new model, added export to script, added to removal of embeddings script

--- training/sentiment/export_model.py
+++ training/sentiment/export_model.py
@@ -28,6 +28,28 @@ from keras import regularizers
 # model.add(Dense(1, activation='sigmoid'))
 
 # Fine grained classifier model
+# model = Sequential()
+# model.add(Embedding(input_length=52, input_dim=3000000, output_dim=300, trainable=False, mask_zero=True))
+# model.add(Conv1D(64, 3, kernel_regularizer=regularizers.l2(1e-4)))
+# model.add(AveragePooling1D(2))
+# model.add(BatchNormalization())
+# model.add(Conv1D(32, 3, activation='relu', kernel_regularizer=regularizers.l2(1e-4)))
+# model.add(LSTM(32, activation='tanh', recurrent_activation='sigmoid', recurrent_dropout=0, unroll=False, use_bias=True))
+# model.add(BatchNormalization())
+# model.add(Dropout(0.25))
+# model.add(Dense(128, activation='relu', kernel_regularizer=regularizers.l2(1e-4)))
+# model.add(Dropout(0.25))
+# model.add(Dense(128, activation='relu', kernel_regularizer=regularizers.l2(1e-4)))
+# model.add(Dropout(0.25))
+# model.add(Dense(64, activation='relu', kernel_regularizer=regularizers.l2(1e-4)))
+# model.add(Dropout(0.25))
+# model.add(Dense(64, activation='relu', kernel_regularizer=regularizers.l2(1e-4)))
+# model.add(Dropout(0.25))
+# model.add(Dense(32, activation='relu', kernel_regularizer=regularizers.l2(1e-4)))
+# model.add(Dropout(0.25))
+# model.add(Dense(5, activation='softmax'))
+
+# Regression model
 model = Sequential()
 model.add(Embedding(input_length=52, input_dim=3000000, output_dim=300, trainable=False, mask_zero=True))
 model.add(Conv1D(64, 3, kernel_regularizer=regularizers.l2(1e-4)))
@@ -47,10 +69,11 @@ model.add(Dense(64, activation='relu', kernel_regularizer=regularizers.l2(1e-4))
 model.add(Dropout(0.25))
 model.add(Dense(32, activation='relu', kernel_regularizer=regularizers.l2(1e-4)))
 model.add(Dropout(0.25))
-model.add(Dense(5, activation='softmax'))
+model.add(Dense(1, activation='relu'))
 
 # file = open("./models/binary/20210219-014937/C-LSTM.json", "w+")
-file = open("./models/binary/20210220-000803/C-LSTM.json", "w+")
+# file = open("./models/binary/20210220-000803/C-LSTM.json", "w+")
+file = open("./models/binary/20210309-011610/C-LSTM.json", "w+")
 file.write(model.to_json())
 
 
--- training/sentiment/remove_embeddings.py
+++ training/sentiment/remove_embeddings.py
@@ -2,6 +2,29 @@ from keras.models import Sequential
 from keras.layers import LSTM, Dropout, Dense, BatchNormalization, Conv1D, AveragePooling1D, Input
 from keras import regularizers
 
+# Binary model
+# model = Sequential()
+# model.add(Input(shape=(None, 300), name='embedding_input'))
+# model.add(Conv1D(64, 3, kernel_regularizer=regularizers.l2(1e-4)))
+# model.add(AveragePooling1D(2))
+# model.add(BatchNormalization())
+# model.add(Conv1D(32, 3, activation='relu', kernel_regularizer=regularizers.l2(1e-4)))
+# model.add(LSTM(32, activation='tanh', recurrent_activation='sigmoid', recurrent_dropout=0, unroll=False, use_bias=True))
+# model.add(BatchNormalization())
+# model.add(Dropout(0.25))
+# model.add(Dense(128, activation='relu', kernel_regularizer=regularizers.l2(1e-4)))
+# model.add(Dropout(0.25))
+# model.add(Dense(128, activation='relu', kernel_regularizer=regularizers.l2(1e-4)))
+# model.add(Dropout(0.25))
+# model.add(Dense(64, activation='relu', kernel_regularizer=regularizers.l2(1e-4)))
+# model.add(Dropout(0.25))
+# model.add(Dense(64, activation='relu', kernel_regularizer=regularizers.l2(1e-4)))
+# model.add(Dropout(0.25))
+# model.add(Dense(32, activation='relu', kernel_regularizer=regularizers.l2(1e-4)))
+# model.add(Dropout(0.25))
+# model.add(Dense(1, activation='sigmoid'))
+
+# Regression model
 model = Sequential()
 model.add(Input(shape=(None, 300), name='embedding_input'))
 model.add(Conv1D(64, 3, kernel_regularizer=regularizers.l2(1e-4)))
@@ -21,11 +44,13 @@ model.add(Dense(64, activation='relu', kernel_regularizer=regularizers.l2(1e-4))
 model.add(Dropout(0.25))
 model.add(Dense(32, activation='relu', kernel_regularizer=regularizers.l2(1e-4)))
 model.add(Dropout(0.25))
-model.add(Dense(1, activation='sigmoid'))
+model.add(Dense(1, activation='relu'))
 
-model.load_weights("./models/binary/20210219-014937/C-LSTM.hdf5", by_name=True)
+# model.load_weights("./models/binary/20210219-014937/C-LSTM.hdf5", by_name=True)
+model.load_weights("./models/binary/20210309-011610/C-LSTM.hdf5", by_name=True)
 model.summary()
 
-file = open("./models/binary/20210219-014937/no_embedding/C-LSTM.json", "w+")
+# file = open("./models/binary/20210219-014937/no_embedding/C-LSTM.json", "w+")
+file = open("./models/binary/20210309-011610/no_embedding/C-LSTM.json", "w+")
 file.write(model.to_json())
-model.save_weights("./models/binary/20210219-014937/no_embedding/C-LSTM.hdf5")
+model.save_weights("./models/binary/20210309-011610/no_embedding/C-LSTM.hdf5")
