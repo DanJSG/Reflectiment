a3d0d660aa73179f4ca8374b783d893046991b35
DanJSG <jackson.daniel365@gmail.com>
2021-03-12 15:25:01 +0000

    MENG-77: Added blank results card, added down arrow, refactored into components

--- web-ui/src/components/NavBar.jsx
+++ web-ui/src/components/NavBar.jsx
@@ -9,9 +9,9 @@ function NavBar() {
         <React.Fragment>
             <nav className="navbar sticky-top navbar-expand-md navbar-dark bg-dark">
                 <a className="navbar-brand border-right border-secondary" href="./">
-                    <img src={UoYLogo} alt="" width="240px"/>
+                    <img src={UoYLogo} alt="" width="180px"/>
                 </a>
-                <div className="navbar-nav" style={{fontSize: "1.3rem"}}>
+                <div className="navbar-nav" style={{fontSize: "1.2rem"}}>
                     <Link className="nav-item nav-link active" to="/">Analyser</Link>
                     <Link className="nav-item nav-link" to="/about">About</Link>
                 </div>
--- web-ui/src/pages/HomePage.jsx
+++ web-ui/src/pages/HomePage.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import NavBar from '../components/NavBar';
 import MainContent from '../components/MainContent';
 import TextSubmissionCard from '../components/TextSubmissionCard';
+import ResultsCard from '../components/ResultsCard';
+import downArrow from '../resources/down.svg'
 
 function HomePage() {
 
@@ -18,6 +20,25 @@ function HomePage() {
                 </p>
                 {/* Submission Form Card */}
                 <TextSubmissionCard />
+                <img className="p-5" src={downArrow} alt="" width="25%"/>
+                <h1 className="pb-3 font-weight-normal">Results</h1>
+                <ResultsCard />
+                {/* <div className="card w-100 p-3 shadow-sm border-1 row">
+                    <div className="container-fluid">
+                        <div className="row">
+                            <div className="col-6 border-right">
+                                <h3 className="font-weight-normal text-center card-title">Average Scores</h3>
+                            </div>
+                            <div className="col-6">
+                                <h3 className="font-weight-normal text-center card-title">Maximum Scores</h3>
+                            </div>
+                        </div>
+                        <hr/>
+                        <div className="row p-3">
+                            <h3 className="font-weight-normal text-center card-title">Tagged Text</h3>
+                        </div>
+                    </div>
+                </div> */}
             </MainContent>
         </div>
     )
