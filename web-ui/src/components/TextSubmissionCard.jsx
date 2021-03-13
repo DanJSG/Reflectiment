6dfa6be5d30f76dde19753332c97d4a3350171c5
DanJSG <jackson.daniel365@gmail.com>
2021-03-13 17:23:04 +0000

    MENG-90: Added loading text and spinner to analysis button whilst waiting for response

--- web-ui/src/components/TextSubmissionCard.jsx
+++ web-ui/src/components/TextSubmissionCard.jsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import {sendAnalysisRequest} from '../services/analysisservice';
 
 function TextSubmissionCard(props) {
+    
+    const loadingButton = useRef(null);
+    const analyzeButton = useRef(null);
 
     const analyseText = async (e) => {
         e.preventDefault();
+        loadingButton.current.style.display = "";
+        analyzeButton.current.style.display = "none";
         let text = e.target.elements.textSubmissionBox.value;
         if(text === null || text === undefined) {
             throw Error("Failed to send analysis request.");
         }
         const response = await sendAnalysisRequest(text.trim());
+        loadingButton.current.style.display = "none";
+        analyzeButton.current.style.display = "";
         props.handleAnalysisResponse(response);
     }
 
@@ -23,7 +30,11 @@ function TextSubmissionCard(props) {
                     <textarea className="form-control" placeholder="Enter your text here..." name="textSubmissionBox" id="textSubmissionBox0" rows="12" style={{resize: "none"}} />
                 </div>
                 <div className="form-group">
-                    <button className="btn btn-primary pl-4 pr-4">Analyze</button>
+                    <button ref={analyzeButton} className="btn btn-primary pl-4 pr-4">Analyze</button>
+                    <button ref={loadingButton} disabled className="btn btn-primary pl-4 pr-4 disabled" style={{display: 'none'}}>
+                        <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true" />
+                        &nbsp;Loading...
+                    </button>
                 </div>
             </form>
         </div>
