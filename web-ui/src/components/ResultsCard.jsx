2385819116990eccac3b06194854b59cc29b2bdc
DanJSG <jackson.daniel365@gmail.com>
2021-03-13 13:19:55 +0000

    MENG-89: Added methods for finding max and average analysis scores

--- web-ui/src/components/ResultsCard.jsx
+++ web-ui/src/components/ResultsCard.jsx
@@ -1,23 +1,144 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function ResultsCard(props) {
 
-    console.log(props.analysis);
+    const [averageScores, setAverageScores] = useState(null);
+    const [maxScores, setMaxScores] = useState(null);
+
+    // TODO refactor into separate service class
+    const getAverageScores = (sentences) => {
+        let sentimentSum = 0;
+        let moodSum = 0;
+        let reflectionSum = 0;
+        let i = 0;
+        for(i; i < sentences.length; i++) {
+            sentimentSum += sentences[i].lexicalScores.sentiment.score;
+            moodSum += sentences[i].lexicalScores.mood.score;
+            reflectionSum += sentences[i].lexicalScores.reflection.score;
+        }
+        return {
+            sentiment: sentimentSum / (parseInt(i) + 1),
+            mood: moodSum / (parseInt(i) + 1),
+            reflection: reflectionSum / (parseInt(i) + 1)
+        }
+    }
+
+    // TODO refactor into separate service class
+    const getMaxScores = (sentences) => {
+        let maxes = {
+            sentiment: -1,
+            mood: 0,
+            moodLabel: "none",
+            reflection: 0
+        }
+        for(let i = 0; i < sentences.length; i++) {
+            maxes.sentiment = sentences[i].lexicalScores.sentiment.score > maxes.sentiment ? sentences[i].lexicalScores.sentiment.score : maxes.sentiment;
+            maxes.moodLabel = sentences[i].lexicalScores.mood.score > maxes.mood ? sentences[i].lexicalScores.mood.label : maxes.moodLabel;
+            maxes.moodLabel = maxes.moodLabel.charAt(0).toUpperCase() + maxes.moodLabel.substr(1);
+            maxes.mood = sentences[i].lexicalScores.mood.score > maxes.mood ? sentences[i].lexicalScores.mood.score : maxes.mood;
+            maxes.reflection = sentences[i].lexicalScores.reflection.score > maxes.reflection ? sentences[i].lexicalScores.reflection.score : maxes.reflection;
+        }
+        return maxes;
+    }
+
+    useEffect(() => {
+        if(!props.analysis) {
+            return;
+        }
+        const averages = getAverageScores(props.analysis.sentences);
+        const maxes = getMaxScores(props.analysis.sentences);
+        console.log(averages);
+        setAverageScores(averages);
+        setMaxScores(maxes);
+    }, [props.analysis])
 
     return (
-        <div className="card w-100 p-3 shadow-sm border-1 row">
-            <div className="container-fluid">
+        <div className="card w-100 shadow-sm border-1 row">
+            <div className="card-header">
+                <h1 className="font-weight-normal">Results</h1>
+                {/* <ul className="nav nav-tabs card-header-tabs">
+                    <li className="nav-item">
+                        <a className="nav-link" href="">Tab 1</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="">Tab 2</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="">Tab 3</a>
+                    </li>
+                </ul> */}
+            </div>
+            <div className="container-fluid p-3">
                 <div className="row">
                     <div className="col-6 border-right">
                         <h3 className="font-weight-normal text-center card-title">Average Scores</h3>
+                        {!averageScores ? null : 
+                            <table className="table">
+                                <thead>
+                                    <tr>
+                                        <th scope="col" style={{width: "33%"}}>Aspect</th>
+                                        <th className="col" style={{width: "33%"}}>Intensity</th>
+                                        <th className="col" style={{width: "33%"}}>Label</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>Sentiment</td>
+                                        <td>{averageScores.sentiment.toFixed(5)}</td>
+                                        <td>N/A</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Mood</td>
+                                        <td>{averageScores.mood.toFixed(5)}</td>
+                                        <td>{averageScores.moodLabel}</td>
+                                    </tr>
+                                    <td>Reflection</td>
+                                    <td>{averageScores.reflection.toFixed(5)}</td>
+                                    <td>N/A</td>
+                                </tbody>
+                            </table>
+                        }
                     </div>
                     <div className="col-6">
                         <h3 className="font-weight-normal text-center card-title">Maximum Scores</h3>
+                        {!maxScores ? null : 
+                            <table className="table">
+                                <thead>
+                                    <tr>
+                                        <th scope="col" style={{width: "33%"}}>Aspect</th>
+                                        <th className="col" style={{width: "33%"}}>Intensity</th>
+                                        <th className="col" style={{width: "33%"}}>Label</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>Sentiment</td>
+                                        <td>{maxScores.sentiment.toFixed(5)}</td>
+                                        <td>N/A</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Mood</td>
+                                        <td>{maxScores.mood.toFixed(5)}</td>
+                                        <td>{maxScores.moodLabel}</td>
+                                    </tr>
+                                    <td>Reflection</td>
+                                    <td>{maxScores.reflection.toFixed(5)}</td>
+                                    <td>N/A</td>
+                                </tbody>
+                            </table>
+                        }
                     </div>
                 </div>
                 <hr/>
                 <div className="row p-3">
-                    <h3 className="font-weight-normal text-center card-title">Tagged Text</h3>
+                    <div className="col-12">
+                        <h2 className="font-weight-normal card-title">Tagged Text</h2>
+                        {!props.analysis ? null :
+                            <div>
+                                <p>{props.analysis.text}</p>
+                            </div>
+                        }
+                    </div>
                 </div>
             </div>
         </div>
--- web-ui/src/pages/HomePage.jsx
+++ web-ui/src/pages/HomePage.jsx
@@ -24,18 +24,19 @@ function HomePage() {
             <NavBar />
             <MainContent>
                 {/* Heading and summary */}
-                <h1 className="p-3 display-4 font-weight-normal">Automated Text Analyzer</h1>
+                <h1 className="p-4 display-4 font-weight-normal">Automated Text Analyzer</h1>
                 <p className="pl-3 pr-3 text-justify">
                     This tool automatically analyzes sentiment, mood, and reflection in a piece of written text. 
                     To get started, simply type some text into the box below and click "Analyze". 
                     The results will then be shown below automatically.
                 </p>
                 <TextSubmissionCard handleAnalysisResponse={handleAnalysisResponse} />
+                <div className="p-4"></div>
                 {
                     analysis === null ? null : 
                     <React.Fragment>
-                        <img className="p-5" src={downArrow} alt="" width="25%"/>
-                        <h1 className="pb-3 font-weight-normal">Results</h1>
+                        {/* <img className="p-5" src={downArrow} alt="" width="25%"/>
+                        <h1 className="pb-3 font-weight-normal">Results</h1> */}
                         <ResultsCard analysis={analysis}/>
                     </React.Fragment>
                 }
