c2da472a9b88e33324721d9a052e340a29da7927
DanJSG <jackson.daniel365@gmail.com>
2021-03-15 12:07:43 +0000

    MENG-91: Extracted tagging logic into separate service file

--- web-ui/src/components/ResultsCard.jsx
+++ web-ui/src/components/ResultsCard.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import {getAverageScores, getMaxScores} from '../services/resultprocessing';
+import {pickTaggingFunction} from '../services/taggingservice';
 import ResultsTable from './ResultsTable';
 
 function ResultsCard(props) {
@@ -41,60 +42,6 @@ function ResultsCard(props) {
             setActiveRadioButton(2);
         }
         tagText(e.target.id.replace("Radio", ''), activeTab);
-    }
-
-    // TODO refactor into new file
-    const tagSentiment = (sentence, scores, index) => {
-        const colorVal = scores.score;
-        const colorStyle = {
-            backgroundColor: colorVal > 0 ? `rgba(0, 255, 0, ${Math.abs(colorVal)})` : `rgb(255, 0, 0, ${Math.abs(colorVal)})`
-        }
-        return <span key={index} style={colorStyle}>{sentence}&nbsp;</span>;
-    }
-
-    // TODO refactor into new file
-    const tagMood = (sentence, scores, index) => {
-        console.log(scores.mixedScores);
-        let colorStyle = {
-            backgroundColor: `rgba(0, 0, 0, 0)`,
-            color: "#212529"
-        };
-        if(scores.label === "anger") {
-            colorStyle.backgroundColor = `rgba(255, 0, 0, ${scores.score})`;
-        } else if(scores.label === "sadness") {
-            colorStyle.backgroundColor = `rgba(0, 0, 255, ${scores.score})`;
-            colorStyle.color = scores.score > 0.5 ? "#f8f9fa" : colorStyle.color;
-        } else if(scores.label === "fear") {
-            colorStyle.backgroundColor = `rgba(128, 0, 128, ${scores.score})`;
-            colorStyle.color = scores.score > 0.5 ? "#f8f9fa" : colorStyle.color;
-        } else {
-            colorStyle.backgroundColor = `rgba(255, 255, 0, ${scores.score})`;
-        }
-        return (
-            <span key={index} style={colorStyle}>{sentence}&nbsp;</span>
-        )
-
-        // console.log(sentence);
-    }
-
-    // TODO refactor into new file
-    // TODO review using a temperature style colouring scheme
-    const tagReflection = (sentence, scores, index) => {
-        const colorVal = scores.score;
-        const colorStyle = {
-            backgroundColor: `rgba(${128 - (colorVal * 95)}, ${128 + (colorVal * 95)}, 0, ${colorVal})`
-        }
-        return <span key={index} style={colorStyle}>{sentence}&nbsp;</span>;
-    }
-    
-    const pickTaggingFunction = (analysisFeature) => {
-        if(analysisFeature === "sentiment") {
-            return tagSentiment;
-        } else if(analysisFeature === "mood") {
-            return tagMood;
-        } else {
-            return tagReflection;
-        }
     }
 
     const tagText = (analysisFeature, tabId) => {
@@ -154,7 +101,6 @@ function ResultsCard(props) {
                         </form>
                         {!props.analysis ? null :
                             <div>
-                                {/* <p>{props.analysis.text}</p> */}
                                 <p>{taggedSentences}</p>
                             </div>
                         }
