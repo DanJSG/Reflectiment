50d37a682d0cbd7109ba7372657e2772da793363
DanJSG <jackson.daniel365@gmail.com>
2021-03-22 12:31:52 +0000

    MENG-98: Commented text submission code (UI component and logic)

--- web-ui/src/components/submission/TextSubmissionCard.jsx
+++ web-ui/src/components/submission/TextSubmissionCard.jsx
@@ -1,6 +1,13 @@
 import React, { useRef, useState } from 'react';
 import {sendAnalysisRequest} from './services/analysisservice';
 
+/**
+ * Component for the text submission card, handling the text area for the text input and 
+ * the submit button.
+ * 
+ * @param {Object} props the component properties passed through from the parent
+ * @returns the relevant DOM elements and event handlers
+ */
 function TextSubmissionCard(props) {
     
     const loadingButton = useRef(null);
@@ -8,6 +15,15 @@ function TextSubmissionCard(props) {
     const [errorMessage, setErrorMessage] = useState(null);
     const [isAnalyzeButtonDisabled, setIsAnalyzeButtonDisabled] = useState(true);
 
+    /**
+     * Handle the text analysis submission. Switches the displayed buttons to
+     * provide the user with visual feedback that the request is loading, and sends the text to
+     * the backend API for analysis. The API's response is then passed back to the parent component,
+     * or if there is an error message, this is displayed.
+     * 
+     * @param {Object} e the form submission event
+     * @returns undefined
+     */
     const analyseText = async (e) => {
         e.preventDefault();
         loadingButton.current.style.display = "";
@@ -29,6 +45,12 @@ function TextSubmissionCard(props) {
         props.handleAnalysisResponse(response);
     }
 
+    /**
+     * Check whether or not the text area is empty. If the text area is empty, then
+     * disable the "Analyze" button. Otherwise, enable the button.
+     * 
+     * @param {Object} e the onChange event from the text area
+     */
     const textAreaUpdated = (e) => {
         if(e.target.value.trim() === "") {
             setIsAnalyzeButtonDisabled(true);
--- web-ui/src/components/submission/services/analysisservice.js
+++ web-ui/src/components/submission/services/analysisservice.js
@@ -1,3 +1,11 @@
+/**
+ * Submits the given text to the document API endpoint which performs the text
+ * analysis. This analysed response is then returned, or an error message is
+ * wrapped in an object and returned.
+ * 
+ * @param {string} text the text to send to the API
+ * @returns {Object} the analysed text or a wrapped error message
+ */
 export const sendAnalysisRequest = async (text) => {
     const url = `http://localhost:8080/api/v1/document`;
     const textSubmission = {
@@ -23,6 +31,13 @@ export const sendAnalysisRequest = async (text) => {
     })
 }
 
+/**
+ * Parses a HTTP response and returns an appropriate error message based on the 
+ * status code.
+ * 
+ * @param {Object} response the HTTP response
+ * @returns {string} the error message
+ */
 const parseError = (response) => {
     if(response.status === 400) {
         return "There seems to be an issue with the submitted text. Please ensure that there is text to be submitted, and that there are no leading full stops, question marks or exclamation marks.";
