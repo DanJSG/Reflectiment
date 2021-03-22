408968244df5b859084e64d5995c015c5095d63a
DanJSG <jackson.daniel365@gmail.com>
2021-03-22 15:08:05 +0000

    MENG-98: Commented global components (navbar, layout)

--- web-ui/src/components/layout/MainContent.jsx
+++ web-ui/src/components/layout/MainContent.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
+/**
+ * Main content layout component, which takes a set of children and wraps them within layout divs.
+ * 
+ * @param {Object} props the component properties passed down from the parent
+ * @returns the DOM elements to render, including the children passed through
+ */
 function MainContent(props) {
     return (
         <div className="container-fluid">
